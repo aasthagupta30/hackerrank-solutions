acb232d08be191394aac84562ddbab7fe33e03d0
AASTHA GUPTA <72187301+aasthagupta30@users.noreply.github.com>
2020-10-19 15:15:42 +0530

    Create celsius_to_fahrenheit.py

---
+++ celsius_to_fahrenheit.py
@@ -0,0 +1,5 @@
+#convert celsius to fahrenheit
+
+celsius = float(input("Enter the temperature in celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print('%.2f celsius = %0.2f fahrenheit' %(celsius, fahrenheit))
